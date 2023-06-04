719792a64ce9b988c1ce52d6e4da6d5837a8e12f
aman <eagleax7@gmail.com>
2023-06-05 03:21:38 +0530

    hotel booking api final commit

---
+++ routes/hotel.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {verifyAdmin} from "../utils/verifyToken.js"
+
+import {
+    createHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+const router = express.Router();
+router.post("/",verifyAdmin ,createHotel);
+router.put("/:id",verifyAdmin, updateHotel);
+
+router.delete("/:id",verifyAdmin, deleteHotel);
+
+router.get("/:id", getHotel);
+router.get("/", getHotels);
+router.get("/countByCity", getHotels);
+router.get("/countByType", getHotels);
+
+export default router;
